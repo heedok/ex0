58823d6f105d0c479e0ae7ac8da34de226b93f00
heedok <lovehatto@gmail.com>
2025-04-27 16:51:07 +0900

    live cam, face and eyes detection

---
+++ 2.py
@@ -0,0 +1,31 @@
+import cv2
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+cap=cv2.VideoCapture(0)  
+# actioncam 연결 사용 예제
+
+while True:
+    ret,frame=cap.read()
+    
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 얼굴 감지
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+    eyes = eye_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50), maxSize=(150, 150))
+
+    # 감지된 얼굴에 사각형 그리기
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 0, 200), 2)
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 0, 50), 2)
+    
+    if not ret:
+        break
+    cv2.imshow('Webcam',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+   
+cap.release()
+cv2.destroyAllWindows()
